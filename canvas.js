086037c5bd08d15c19e73ae41f77a4b579dbcdff
lmmc2 <lmmc2@cin.ufpe.br>
2017-10-01 17:37:41 -0300

    first commit

---
+++ canvas.js
@@ -0,0 +1,52 @@
+function resizeCanvas(width, height) {
+    canvas.width = width;
+    canvas.height = height;
+}
+
+function resizeToFit() {
+    var width = parseFloat(window.getComputedStyle(canvas).width);
+    var height = parseFloat(window.getComputedStyle(canvas).height);
+    resizeCanvas(width, height);
+}
+
+function draw(){
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    for(var i = 0; i < p_stack.length; i++){
+        ctx.beginPath();
+        ctx.strokeStyle = "black";
+        ctx.fillStyle = "orange";
+        ctx.arc(p_stack[i].x, p_stack[i].y, 3, 0, 2*Math.PI);
+        ctx.stroke();
+        ctx.fill();
+    }
+}
+
+var canvas = document.getElementById('canvas');
+var ctx = canvas.getContext('2d');
+var p_stack = [];
+var pixel = {
+    x: "",
+    y: ""
+};
+var enableInsert = false;
+
+resizeToFit();
+function insert(){
+    enableInsert = true;
+}
+
+function del(){
+    p_stack.pop();
+    draw();
+}
+
+canvas.addEventListener("click", function(e) {
+    if(enableInsert){
+        pixel.x = e.offsetX;
+        pixel.y = e.offsetY;
+        p_stack.push(pixel);
+        draw();
+    }
+    enableInsert = false;
+});
+
