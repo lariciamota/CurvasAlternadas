7dfbe3540bddd4acdde4e1030254d489b797b999
lmmc2 <lmmc2@cin.ufpe.br>
2017-10-01 21:11:52 -0300

    inserir/deletar/modificar funcionando

--- canvas.js
+++ canvas.js
@@ -15,36 +15,41 @@ function draw(){
         ctx.beginPath();
         ctx.strokeStyle = "black";
         ctx.fillStyle = "orange";
-        ctx.arc(p_stack[i].x, p_stack[i].y, p_stack[i].radius, 0, 2*Math.PI);
+        ctx.arc(p_stack[i].x, p_stack[i].y, 5, 0, 2*Math.PI);
         ctx.stroke();
         ctx.fill();
     }
+    for(var j = 0; j < p_stack.length-1; j++){
+        ctx.beginPath();
+        ctx.strokeStyle = "black";
+        ctx.moveTo(p_stack[j].x, p_stack[j].y);
+        ctx.lineTo(p_stack[j+1].x, p_stack[j+1].y);
+        ctx.stroke();
+    }
+
 }
 
 var canvas = document.getElementById('canvas');
 var ctx = canvas.getContext('2d');
 var p_stack = [];
-var point = {
-    x: "",
-    y: "",
-    radius: 3
-};
-var enableInsert = false;
-var move = point;
+var move = null;
 
 resizeToFit();
-function insert(){
-    enableInsert = true;
-}
 
-function del(){
-    p_stack.pop();
-    draw();
-}
+canvas.addEventListener("click", function(e) {
+    if(findPoint(e) === null){
+        var d = {
+            x: "",
+            y: ""
+        };
+        d.x = e.offsetX;
+        d.y = e.offsetY;
+        p_stack.push(d);
+        draw();
+    }
 
-function modify() {
+});
 
-}
 
 function findPoint(click){
     for(var i = 0; i <p_stack.length; i++){
@@ -52,45 +57,34 @@ function findPoint(click){
             x: p_stack[i].x - click.x,
             y: p_stack[i].y - click.y
         };
-        if(Math.sqrt(v.x * v.x + v.y * v.y) <=3){
-            return p_stack[i];
+        if(Math.sqrt(v.x * v.x + v.y * v.y) <= 5){
+            return p_stack[i]; //retornar o ponto que foi clicado e sera movido
         }
     }
     return null;
 }
 
+canvas.addEventListener("dblclick", function(e){
+    var x = findPoint(e);
+    if(x !== null){
+        p_stack.splice(p_stack.indexOf(x), 1);
+        draw();
+    }
+});
+
 canvas.addEventListener('mousedown', function(e) {
-    move = findPoint({
-        x: e.offsetX,
-        y: e.offsetY
-    });
+    move = findPoint(e); //verificar se a coord clicada corresponde a algum ponto
 });
 
 canvas.addEventListener('mousemove', function(e) {
-    if (move !== null) {
+    if (move !== null) { //se foi clicado num ponto existente, atualiza nova coord
         move.x = e.offsetX;
         move.y = e.offsetY;
         draw();
     }
 });
 
-canvas.addEventListener('mouseup', function(e) {
-    move = false;
-});
-
-
-
-
-
-
-
-canvas.addEventListener("click", function(e) {
-    if(enableInsert){
-        point.x = e.offsetX;
-        point.y = e.offsetY;
-        p_stack.push(point);
-        draw();
-    }
-    enableInsert = false;
+canvas.addEventListener('mouseup', function() {
+    move = null;
 });
 
