389b09028467f95b2165fd570b0e6022994b3111
lmmc2 <lmmc2@cin.ufpe.br>
2017-10-01 21:12:28 -0300

    inserir/deletar/modificar funcionando

--- canvas.js
+++ canvas.js
@@ -15,29 +15,29 @@ function draw(){
         ctx.beginPath();
         ctx.strokeStyle = "black";
         ctx.fillStyle = "orange";
-        ctx.arc(p_stack[i].x, p_stack[i].y, 3, 0, 2*Math.PI);
+        ctx.arc(p_stack[i].x, p_stack[i].y, 5, 0, 2*Math.PI);
         ctx.stroke();
         ctx.fill();
     }
+    for(var j = 0; j < p_stack.length-1; j++){
+        ctx.beginPath();
+        ctx.strokeStyle = "black";
+        ctx.moveTo(p_stack[j].x, p_stack[j].y);
+        ctx.lineTo(p_stack[j+1].x, p_stack[j+1].y);
+        ctx.stroke();
+    }
+
 }
 
 var canvas = document.getElementById('canvas');
 var ctx = canvas.getContext('2d');
 var p_stack = [];
-var enableInsert = false;
+var move = null;
 
 resizeToFit();
-function insert(){
-    enableInsert = true;
-}
-
-function del(){
-    p_stack.pop();
-    draw();
-}
 
 canvas.addEventListener("click", function(e) {
-    if(enableInsert){
+    if(findPoint(e) === null){
         var d = {
             x: "",
             y: ""
@@ -47,6 +47,44 @@ canvas.addEventListener("click", function(e) {
         p_stack.push(d);
         draw();
     }
-    enableInsert = false;
+
+});
+
+
+function findPoint(click){
+    for(var i = 0; i <p_stack.length; i++){
+        var v = {
+            x: p_stack[i].x - click.x,
+            y: p_stack[i].y - click.y
+        };
+        if(Math.sqrt(v.x * v.x + v.y * v.y) <= 5){
+            return p_stack[i]; //retornar o ponto que foi clicado e sera movido
+        }
+    }
+    return null;
+}
+
+canvas.addEventListener("dblclick", function(e){
+    var x = findPoint(e);
+    if(x !== null){
+        p_stack.splice(p_stack.indexOf(x), 1);
+        draw();
+    }
+});
+
+canvas.addEventListener('mousedown', function(e) {
+    move = findPoint(e); //verificar se a coord clicada corresponde a algum ponto
+});
+
+canvas.addEventListener('mousemove', function(e) {
+    if (move !== null) { //se foi clicado num ponto existente, atualiza nova coord
+        move.x = e.offsetX;
+        move.y = e.offsetY;
+        draw();
+    }
+});
+
+canvas.addEventListener('mouseup', function() {
+    move = null;
 });
 
